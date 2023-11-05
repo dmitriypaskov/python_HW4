e3c8e113bd08c05932aa7273681476f356f2a8bf
Dmytro Pashkov <dmitriypashkovs@gmail.com>
2023-11-05 12:25:59 +0200

    Done

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 #Програма має виконувати прості математичні дії (+, -, *, /).
 #Користувачеві пропонується почерзі ввести числа та дію над цими числами,
 #а програма, виходячи з дії, обчислює та друкує результат.
-#Зробити перевірку на те, що при діленні дільник не дорівнює 0!
-#Приклад:
-#Please enter first number: 3
-#Please enter second number: 5
-#Please enter action: +
-#Your result is 8
-#Приклад с діленням на нуль:
-#Please enter first number: 10
-#Please enter second number: 0
-#Please enter action: /
-#You can't divide by 0'
 a = int(input('Please enter first number: '))
 b = int(input('Please enter second number: '))
 act = input('Please enter action: ')
